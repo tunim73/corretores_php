c7104cdee9c702babf6892cc48f621204636228b
tunim73 <antonio.netto973@gmail.com>
2024-02-06 17:40:32 -0300

    frontend start

---
+++ frontend/src/pages/App/index.tsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div className="">
+      <h1>Cadastro de Corretor</h1>
+
+      <button className="bg-red-500 border-2 border-black px-2 py-1">
+        Enviar
+      </button>
+    </div>
+  );
+}
+
+export default App;
